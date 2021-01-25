7d792c41b88471b223f2305c3545c8784df51052
Angel Dimitrov <angel.k.dimitrov@gmail.com>
2021-01-25 17:38:55 +0200

    fix

--- Tests/Lib/Pages/skinnyPage.js
+++ Tests/Lib/Pages/skinnyPage.js
@@ -6,6 +6,15 @@ get cookieClose() {
 get findOutMoreButton() {
     return $('#myCarousel > div > div.active.item > div.find-out')
 }
+get logIn() {
+    return $('#new-login-style > a > input')
+}
+get typesButton() {
+    return $('#display-Types')
+}
+get groupsButton() {
+    return $('#display-Groups')
+}
 closeCookiePopup () {
     this.cookieClose.waitForExist()
     this.cookieClose.click()
@@ -13,6 +22,17 @@ closeCookiePopup () {
 clickfindOutMore () {
     this.findOutMoreButton.waitForExist()
     this.findOutMoreButton.click()
+}
+logInButton () {
+    this.logIn.waitForExist()  
+}
+clickTypes () {
+    this.typesButton.waitForExist()
+    this.typesButton.click()
+}
+clickGroups () {
+    this.groupsButton.waitForExist()
+    this.groupsButton.click()
 }
 }
 export default new skinnyPage()
--- Tests/skinnyPage.test.js
+++ Tests/skinnyPage.test.js
@@ -6,27 +6,28 @@ describe('should open SP7 and test skinny page ', () => {
         browser.url(livetUrl);
         browser.maximizeWindow();
         skinnyPage.closeCookiePopup();
-        expect(browser).toHaveUrl('https://www.segmentationportal.com/Mosaic7');
-        
+        expect(browser).toHaveUrl(livetUrl);
     });
+
     it('should  navigate to skunny page ', () => {
-        const loginButton = $('#new-login-style > a > input');
         skinnyPage.clickfindOutMore()
         expect(browser).toHaveTitle('Segments');
-        expect(loginButton).toBeVisible();
-       
+        //Assert thet logIn buuton exist
+        skinnyPage.logInButton() 
     });
-    it('should reorder Mosaics', () => {
-        const typeButon = $('#display-Types')
-        typeButon.waitForExist();
-        typeButon.click();
-        $('#Children').click();
-        $('#Residency').click();
-        $('#display-Groups').click(); 
-        $('#Age').click();
 
+    it('should reorder Mosaics', () => {
+       let experianLogo = $('#experian-logo')
+       let reorderByChildren = $('#Children');
+       let reorderByAge = $('#Age');
+       skinnyPage.clickTypes()
+       reorderByChildren.click();
+       skinnyPage.clickGroups();
+       reorderByAge.click();
+       expect(experianLogo).toBeDisplayed()
     });
-    xit('should navigate to B group Panels page ', () => {
+
+    xit('should navigate to B group Panels page and click on 3 info tooltips ', () => {
         const groupB = $('#B');
         groupB.waitForExist();
         groupB.click();
@@ -34,36 +35,53 @@ describe('should open SP7 and test skinny page ', () => {
         $('//*[@id="question-panel4"]/a/img').click();
         $('//*[@id="question-panel5"]/a/img').click();
         $('#box-1').click();
+        //add asertion
     });
     xit('should navigate to Description page  ', () => {
-        const description_button = $("/html/body/div[4]/div[1]/div[4]/div[1]/div[2]")
+        const description_button = $("//body/div[6]/div[1]/div[4]/div[1]/div[2]")
+        description_button.waitForExist
         description_button.click();
         $('#wordcloud_text_6').click
+        description_button.waitForExist
         description_button.click();
         $('#wordcloud_text_15').click();
+        description_button.waitForExist
         description_button.click();
         $('#wordcloud_text_20').click();
+        //add asertion
     });
     xit('should navigate to Location page  ', () => {
-        const location_button = $("/html/body/div[4]/div[1]/div[4]/div[1]/div[3]")
+        const location_button = $("//body/div[6]/div[1]/div[4]/div[1]/div[3]")
         location_button.click();
         const zoomIn = $('//*[@id="mymap"]/div[2]/div[1]/div/a[1]');
         const zoomOut = $('//*[@id="mymap"]/div[2]/div[1]/div/a[2]');
         zoomIn.click();
         zoomOut.click();
+        //add asertion
         browser.saveScreenshot('map.png')
     });
     xit('should navigate to Family Tree page  ', () => {
-        const family_tree_button = $("/html/body/div[4]/div[1]/div[4]/div[1]/div[4]")
+        const family_tree_button = $("//body/div[6]/div[1]/div[4]/div[1]/div[4]")
         family_tree_button.click();
+        //add asertion
+
     });
     xit('should navigate to Photos page ', () => {
-        const photos_button = $("/html/body/div[4]/div[1]/div[4]/div[1]/div[5]")
+        const photos_button = $("//body/div[6]/div[1]/div[4]/div[1]/div[5]")
         photos_button.click();
+        //add asertion
     });
     xit('should navigate to Data page ', () => {
-        const data_button = $("/html/body/div[4]/div[1]/div[4]/div[1]/div[6]")
+        const data_button = $("//body/div[6]/div[1]/div[4]/div[1]/div[6]")
+        const top100Button = $("#showtop100Insights")
+        const searchField = $("#searchInput")
+        data_button.waitForExist
         data_button.click();
-        browser.pause(3000);
+        top100Button.waitForExist
+        top100Button.click
+        searchField.setValue('family ')
+        $("//body/div[@id='data-page']/div[1]/div[4]/div[4]/div[1]/div[3]").click()
+
+        //browser.pause(3000);
     });
 });
